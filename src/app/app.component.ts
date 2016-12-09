6067b4414adc0ab1a43fd3d69cf250fd08b98d49
haingoarilala <randrianjatovomonjyhaingoarilala@yahoo.fr>
2016-12-09 14:47:07 +0300

    form login and form validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+   formGroup: FormGroup;
+  constructor(formBuilder: FormBuilder) {
+    this.formGroup=formBuilder.group({
+        'name': [null, Validators.required],
+        'password':[null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+        'remember':true
+    });
+  }
+submitForm(value: any):void{
+    console.log('Reactive Form Data: ')
+    console.log(value);
+  }
+   
 }

873cd183bcdaf6935183631cd2e5bb400307900b
haingoarilala <randrianjatovomonjyhaingoarilala@yahoo.fr>
2016-12-22 14:39:07 +0300

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{372:function(n,o){},665:function(n,o,c){n.exports=c(372)}},[665]);
+webpackJsonp([1,2],{373:function(n,o){},671:function(n,o,c){n.exports=c(373)}},[671]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
